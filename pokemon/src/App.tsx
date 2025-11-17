f0fb97497fec15d56b5f82925d3831bb63ba768a
Laura <laura.aucher@vivaticket.com>
2025-11-17 14:26:46 +0100

    exercice

--- pokemon/src/App.tsx
+++ pokemon/src/App.tsx
@@ -1,21 +1,56 @@
-const fetchRandomPokemon = async () => {
-  const randomId = Math.floor(Math.random() * 898) + 1; // ID Pokémon de 1 à 898
-  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomId}`);
-  const data = await res.json();
-  return { name: data.name, sprite: data.sprites.front_default };
-};
+import { useEffect, useState } from "react"
 
-const fetchSpecificPokemon = async (ID: string) => {
-  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${ID}`);
-  const data = await res.json();
-  return { name: data.name, sprite: data.sprites.front_default };
+const fetchRandomPokemon = async () => {
+  const randomId = Math.floor(Math.random() * 898) + 1 // ID Pokémon de 1 à 898
+  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomId}`)
+  const data = await res.json()
+  return { name: data.name, sprite: data.sprites.front_default }
 }
 
+const fetchSpecificPokemon = async (ID: string) => {
+  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${ID}`)
+  const data = await res.json()
+  return { name: data.name, sprite: data.sprites.front_default }
+}
 
 export default function Pokedex() {
+  const [pokemons, setPokemons] = useState([])
+
+  const fetchPokemons = async () => {
+    const randomPokemon1 = await fetchRandomPokemon()
+    const randomPokemon2 = await fetchRandomPokemon()
+    setPokemons([randomPokemon1, randomPokemon2])
+  }
+
+  useEffect(() => {
+    fetchPokemons()
+  }, [])
+
+  async function AddPokemon(e) {
+    e.preventDefault()
+    const form = new FormData(e.target)
+    const id = form.get("id")
+    const pokemon = await fetchSpecificPokemon(id)
+    setPokemons(prevValue => [...prevValue, pokemon])
+  }
+
   return (
     <div style={{ padding: 20 }}>
       <h2 className="pb-12">Mini Pokédex</h2>
+      <form onSubmit={AddPokemon}>
+        <div>
+          <input name="id" type="number" />
+          <button>ajouter un pokemon</button>
+        </div>
+      </form>
+      <ul>
+        {pokemons.map(pokemon => (
+          <li>
+            <img alt="" src={pokemon.sprite} />
+            <p>{pokemon.name}</p>
+          </li>
+        ))}
+      </ul>
     </div>
-  );
+  )
 }
--- todo-list/src/App.tsx
+++ todo-list/src/App.tsx
@@ -1,9 +1,34 @@
-function App() {
-  return (
-    <div>
-      <h1>Hello world</h1>
-    </div>
-  );
+import { useState } from "react"
+function TodoItem({ item }) {
+  return <li>{item}</li>
 }
 
-export default App;
+function App() {
+  const [todolist, setTodolist] = useState<string[]>([])
+  const [value, setValue] = useState("")
+
+  const handleClick = () => {
+    setTodolist(prevTodo => {
+      return [...prevTodo, value]
+    })
+  }
+
+  return (
+    <div>
+      <h1>Add a new item on list</h1>
+      <input
+        type="text"
+        value={value}
+        onChange={e => setValue(e.target.value)}
+      />
+      <button onClick={handleClick}> Ajouter</button>
+      <ul>
+        {todolist.map((item: any) => (
+          <TodoItem item={item} />
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default App
